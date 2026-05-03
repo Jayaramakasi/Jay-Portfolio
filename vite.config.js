53cd139f04081d208d6ad13b56cc32ae5ef9652b
KASI JAYA RAMARAO <20255-cm-025@aec.edu.in>
2026-05-03 11:25:45 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Jayaramarao-J-Portfolio/', 
+  base: './', 
   plugins: [react()],
 })
